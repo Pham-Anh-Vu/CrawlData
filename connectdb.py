ceb34074e4a27fb35df70a0ed2900f31b84b785e
HiimMinDuck <87627835+HiimMinDuck@users.noreply.github.com>
2023-03-15 08:55:10 +0700

    Update connectdb.py

--- connectdb.py
+++ connectdb.py
@@ -9,10 +9,10 @@ def connect():
     try:
         conn = mariadb.connect(
             user="root",
-            password="123123",
+            password="Nmd021200.",
             host="127.0.0.1",
             port=3306,
-            database="abc"
+            database="test"
         )
         return conn
     except mariadb.Error as e:
